61734cbbd9b723680cfbd1da862025eda96abbd1
Yetunde-Iyanu <awoladeyetunde60@gmail.com>
2025-03-04 01:30:46 +0100

    working on javascript

---
+++ DataTypes.js
@@ -0,0 +1,34 @@
+// 1. Declaring a variable//
+let firstName = "Yetunde";
+let lastName = "Awolade";
+let country = "Nigeria";
+let city = "Lagos";
+let age = 29;
+let isMarried = true;
+let year = 2025;
+
+console.log(firstName)
+console.log(lastName)
+console.log(country)
+console.log(city)
+console.log(age)
+console.log(isMarried)
+console.log(year)
+
+//2. //
+console.log(typeof '10'== typeof 10);
+
+//3. parselent //
+console.log(parseInt('9.8') == 10);
+
+//4.boolean // TRUTHY
+console.log(Boolean(10));
+console.log(Boolean("frontend web"));
+console.log(Boolean(['']));
+//FALSY//
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+//5.//
+4>3 && 10<12 //true//
+console.log()
